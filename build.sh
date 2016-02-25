9d6679ad99f334589d26b7dd02314c88aae28418
Mans Matulewicz <cybermans@gmail.com>
2016-02-25 15:18:43 +0100

    alleen bouwen wanneer er nog geen build is van deze commit

--- build.sh
+++ build.sh
@@ -5,6 +5,13 @@ mkdir -p $GOPATH/src/github.com/hashicorp/
 cd $GOPATH/src/github.com/hashicorp/
 git clone https://github.com/hashicorp/nomad.git
 cd nomad
+GITCOM=$(git rev-parse --verify HEAD)
+OUTFILE="/out/nomad.${GITCOM:0:7}.zip"
+if [ -f $OUTFILE ]
+  then
+  echo "bestaat al"
+  exit 1;
+fi
 make bootstrap
 make bin
-cp /go/src/github.com/hashicorp/nomad/pkg/linux_amd64.zip /out/nomad.zip
+cp /go/src/github.com/hashicorp/nomad/pkg/linux_amd64.zip $OUTFILE
